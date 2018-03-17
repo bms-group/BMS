4df6d7594b344ad6484e2ca44cc8c2a8821212ef
373616511 <373616511@qq.com>
2018-03-17 11:26:30 +0800

    修改属性文件名称

--- src/main/java/com/asyf/bms/modules/sys/listener/WebContextListener.java
+++ src/main/java/com/asyf/bms/modules/sys/listener/WebContextListener.java
@@ -6,12 +6,13 @@ import com.asyf.bms.modules.sys.service.SystemService;
 import org.springframework.web.context.WebApplicationContext;
 
 public class WebContextListener extends org.springframework.web.context.ContextLoaderListener {
-	
-	@Override
-	public WebApplicationContext initWebApplicationContext(ServletContext servletContext) {
-		if (!SystemService.printKeyLoadMessage()){
-			return null;
-		}
-		return super.initWebApplicationContext(servletContext);
-	}
+
+    @Override
+    public WebApplicationContext initWebApplicationContext(ServletContext servletContext) {
+        //打印欢迎语
+        if (!SystemService.printKeyLoadMessage()) {
+            return null;
+        }
+        return super.initWebApplicationContext(servletContext);
+    }
 }
